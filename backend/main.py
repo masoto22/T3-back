c675a78687cac2bd89a29a6ffa171a6888875f51
Marcela Antonia Soto Gómez <masoto22@uc.cl>
2024-11-08 02:05:55 -0300

    conexión con front deploy

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 # https://medium.com/@timnirmal/stream-openai-respond-through-fastapi-to-next-js-f5395f69687c
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
